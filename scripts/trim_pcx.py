44ecfb7d383520fdbd34dd5fb618c971b6e93440
kudgel <74338630+kudgel@users.noreply.github.com>
2020-11-12 14:55:25 -0700

    PCX will have internal palettes

--- scripts/trim_pcx.py
+++ scripts/trim_pcx.py
@@ -3,5 +3,8 @@ import sys
 
 all = sys.stdin.buffer.read()
 
-# Trim PCX header and palette
-sys.stdout.buffer.write(all[128:-769])
+# Pre-shift the palette
+all[:-768] = [x>>2 for x in all[:-768]]
+
+# Trim PCX header only (leave palette)
+sys.stdout.buffer.write(all[128:])
--- scripts/vgapal.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-import sys
-pal = sys.stdin.buffer.read()[13:]
-sys.stdout.buffer.write(bytes([x>>2 for x in pal]))
